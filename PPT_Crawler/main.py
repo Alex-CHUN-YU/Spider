ce8f636c7012e6878ffa4cbc391a59304320c98b
Alex-CHUN-YU <P76064538@mail.ncku.edu.tw>
2017-07-25 11:03:44 +0800

    remove register file

--- PPT_Crawler/main.py
+++ PPT_Crawler/main.py
@@ -4,7 +4,7 @@ from ptt_crawler import Crawler
 
 def main():
 	crawler = Crawler()
-	crawler.crawl(board = "Beauty", start_page = 3, end_page = 4)
+	crawler.crawl(board = "Beauty", start_page = 1, end_page = 4)
 
 if __name__ == '__main__':
 	main()
